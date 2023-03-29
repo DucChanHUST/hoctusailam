e20a3b4c618309887052276c8176330c27c76555
DucChanHUST <kuntakufcvn@gmail.com>
2023-03-29 07:00:46 +0700

    ok

--- NLHDH/tu dien/tudien.c
+++ NLHDH/tu dien/tudien.c
@@ -21,6 +21,23 @@ node* newNode(char data[], int page, int line){
     return p;
 }
 
+node* insertNext(node* head, char data[], int page, int line){
+    node* current = newNode(data, page, line);
+    if (head == NULL) return current;
+    node* last = head;
+    while (last->next != NULL)
+        last = last->next;
+    last->next = current;
+    return head;
+}
+
+node* insertDown(node* head, char data[], int page, int line){
+    node* current = newNode(data, page, line);
+    if (head == NULL) return current;
+    head->down = current;
+    return current;
+}
+
 void printList(node* head){
     node* pnext;
     node* pdown = head;
@@ -37,30 +54,50 @@ void printList(node* head){
     }
 }
 
-
-
+node* findNode(node* head, char data[]){
+    node* current = head;
+    while (current != NULL)
+    {
+        if (current->data == data)
+            return current;
+        current = current->down;
+    }
+    return NULL;
+}
 
 int main(){
     FILE *fp = NULL;
     char text[12] = {0};
     fopen_s(&fp, "TestFile.txt", "r");
 
-    // int k = 1;
-    // while (fscanf(fp, "%s", &text) != EOF && k < 10)
-    // {
-    //     if (text[strlen(text)-1] == ',' || text[strlen(text)-1] == '.')
-    //         text[strlen(text)-1] = '\0';
-    //     strlwr(text);
-    //     if (strlen(text) > k)
-    //     {
-    //         k = strlen(text);
-    //     }
+    node* head = NULL, head2 = NULL;
+    int k = 1;
+    while (fscanf(fp, "%s", &text) != EOF && k < 10)
+    {
+        if (text[strlen(text)-1] == ',' || text[strlen(text)-1] == '.')
+            text[strlen(text)-1] = '\0';
+        strlwr(text);
+
+        if (findNode(head, text) == NULL)
+        {
+            
+        }
         
-    //     printf("%s  ", text);
-    // }
+        k++;
+    }
 
     // *** test ***
-
+    // node* aa = newNode("A", 1, 1);
+    // node* ab = newNode("A", 7, 8);
+    // node* ac = newNode("A", 1, 1);
+    // node* ba = newNode("B", 5, 2);
+    // node* bb = newNode("B", 1, 1);
+    // node* ca = newNode("C", 1, 1);
+    // aa->next = ab; 
+    // ab->next = ac;
+    // aa->down = ba;
+    // ba->next = bb;
+    // ba->down = ca;
 
 
 
